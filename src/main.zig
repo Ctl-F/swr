b0c1cf93fc4cb7d5622701f2924783bec4157603
Ctl-F <broughspencer@gmail.com>
2025-10-02 15:29:35 -0600

    surfaces

--- src/main.zig
+++ src/main.zig
@@ -10,16 +10,26 @@ const MyContext = struct {
     should_run: bool,
     x: u32 = 0,
     y: u32 = 0,
+    flip: bool = false,
     axis_keys: [4]bool = [_]bool{false} ** 4,
 };
 
 fn handle_key(ctx: *MyContext, key: u32, pressed: bool) void {
     switch (key) {
-        swr.KeyMap.A => ctx.axis_keys[LEFT] = pressed,
-        swr.KeyMap.D => ctx.axis_keys[RIGHT] = pressed,
-        swr.KeyMap.W => ctx.axis_keys[UP] = pressed,
-        swr.KeyMap.S => ctx.axis_keys[DOWN] = pressed,
+        @as(u32, @intFromEnum(swr.KeyMap.A)) => ctx.axis_keys[LEFT] = pressed,
+        @as(u32, @intFromEnum(swr.KeyMap.D)) => ctx.axis_keys[RIGHT] = pressed,
+        @as(u32, @intFromEnum(swr.KeyMap.W)) => ctx.axis_keys[UP] = pressed,
+        @as(u32, @intFromEnum(swr.KeyMap.S)) => ctx.axis_keys[DOWN] = pressed,
         else => {},
+    }
+
+    if (pressed) {
+        if (key == @as(u32, @intFromEnum(swr.KeyMap.A))) {
+            ctx.flip = true;
+        }
+        if (key == @as(u32, @intFromEnum(swr.KeyMap.D))) {
+            ctx.flip = false;
+        }
     }
 }
 
@@ -107,17 +117,35 @@ pub fn main() !void {
         const v_vel = @as(i32, @intFromBool(context.axis_keys[DOWN])) -
             @as(i32, @intFromBool(context.axis_keys[UP]));
 
-        context.x += h_vel;
-        context.y += v_vel;
+        var new_x: i64 = @as(i64, @intCast(@as(i32, @intCast(context.x)) + h_vel));
+        var new_y: i64 = @as(i64, @intCast(@as(i32, @intCast(context.y)) + v_vel));
+
+        new_x = std.math.clamp(new_x, 0, @as(i64, @intCast(pipeline.framebuffer.size.width - 10)));
+        new_y = std.math.clamp(new_y, 0, @as(i64, @intCast(pipeline.framebuffer.size.height - 10)));
+
+        context.x = @as(u32, @intCast(new_x));
+        context.y = @as(u32, @intCast(new_y));
 
         const fb = pipeline.get_framebuffer();
-        @memset(fb.buffer, PipelineType.Pixel{ .r = 20, .g = 10, .b = 10, .a = 255 });
 
-        fb.buffer[fb.index(context.x + 5, context.y)] = .{ .r = 255, .g = 255, .b = 255, .a = 255 };
-        fb.buffer[fb.index(context.x + 10, context.y)] = .{ .r = 255, .g = 255, .b = 255, .a = 255 };
+        const split_index = fb.index(pipeline.framebuffer.size.width, pipeline.framebuffer.size.height / 2 + pipeline.framebuffer.size.height / 4);
+        const sky = fb.buffer[0..split_index];
+        const ground = fb.buffer[split_index..];
+
+        @memset(sky, PipelineType.Pixel{ .r = 200, .g = 240, .b = 250, .a = 255 });
+        @memset(ground, PipelineType.Pixel{ .r = 20, .g = 100, .b = 50, .a = 255 });
+
+        const offset: u32 = if (context.flip) 1 else 0;
 
         for (0..10) |x| {
-            fb.buffer[fb.index(context.x + @as(u32, @truncate(x)), context.y + 5)] = .{ .r = 255, .g = 255, .b = 255, .a = 255 };
+            for (0..10) |y| {
+                fb.buffer[fb.index(3 + @as(u32, @truncate(x)), 3 + @as(u32, @truncate(y)))] = .{ .r = 255, .g = 255, .b = 150, .a = 255 };
+
+                if ((offset == 1 and x == 0 and y == 0) or (offset == 0 and x == 9 and y == 0)) continue;
+                const color: PipelineType.Pixel = if ((x == 0 or x == 9 or y == 0 or y == 9) or ((x == 3 - offset or x == 7 - offset) and y == 3) or (x > 2 - offset - offset and x < 9 - offset - offset and y == 7)) .{ .r = 0, .g = 0, .b = 0, .a = 255 } else .{ .r = 255, .g = 255, .b = 255, .a = 255 };
+
+                fb.buffer[fb.index(context.x + @as(u32, @truncate(x)), context.y + @as(u32, @truncate(y)))] = color;
+            }
         }
 
         pipeline.refresh();
--- src/root.zig
+++ src/root.zig
@@ -2,11 +2,60 @@ const std = @import("std");
 const sdl = @cImport(@cInclude("SDL3/SDL.h"));
 
 pub const Dim = struct {
+    hor: u32,
+    vert: u32,
+
+    pub inline fn area(this: @This()) u32 {
+        return this.hor * this.vert;
+    }
+};
+
+pub const Rect = struct {
+    x: u32,
+    y: u32,
     width: u32,
     height: u32,
 
     pub inline fn area(this: @This()) u32 {
         return this.width * this.height;
+    }
+
+    pub fn overlaps(this: @This(), other: @This()) @This() {
+        const this_x1 = this.x;
+        const this_y1 = this.y;
+        const this_x2 = this.x + this.width;
+        const this_y2 = this.y + this.height;
+        const other_x1 = other.x;
+        const other_y1 = other.y;
+        const other_x2 = other.x + other.width;
+        const other_y2 = other.y + other.height;
+
+        return this_x1 <= other_x2 and this_x2 >= other_x1 and this_y1 <= other_y2 and this_y2 >= other_y1;
+    }
+
+    pub fn get_overlaping_rect(this: @This(), other: @This()) @This() {
+        std.debug.assert(this.overlaps(other));
+
+        const this_x1 = this.x;
+        const this_y1 = this.y;
+        const this_x2 = this.x + this.width;
+        const this_y2 = this.y + this.height;
+        const other_x1 = other.x;
+        const other_y1 = other.y;
+        const other_x2 = other.x + other.width;
+        const other_y2 = other.y + other.height;
+
+        const new_x1 = @max(this_x1, other_x1);
+        const new_y1 = @max(this_y1, other_y1);
+        const new_x2 = @min(this_x2, other_x2);
+        const new_y2 = @min(this_y2, other_y2);
+
+        return .{
+            .x = new_x1,
+            .y = new_y1,
+            .width = new_x2 - new_x1,
+            .height = new_y2 - new_y1,
+        };
     }
 };
 
@@ -126,7 +175,7 @@ pub fn create_pipeline(comptime pipelineInfo: PipelineInfo) type {
             }
             errdefer sdl.SDL_Quit();
 
-            const window = sdl.SDL_CreateWindow(info.title, @intCast(info.size.width * info.scale), @intCast(info.size.height * info.scale), 0);
+            const window = sdl.SDL_CreateWindow(info.title, @intCast(info.size.hor * info.scale), @intCast(info.size.vert * info.scale), 0);
             if (window == null) {
                 return HostError.CouldNotCreateWindow;
             }
@@ -138,13 +187,13 @@ pub fn create_pipeline(comptime pipelineInfo: PipelineInfo) type {
             }
 
             const staging_buffer = if (comptime FramebufferType != Pixel_RGBA32) BUFFER: {
-                const n_staging_buffer = sdl.SDL_CreateSurface(@intCast(info.size.width), @intCast(info.size.height), sdl.SDL_PIXELFORMAT_RGBA32);
+                const n_staging_buffer = sdl.SDL_CreateSurface(@intCast(info.size.hor), @intCast(info.size.vert), sdl.SDL_PIXELFORMAT_RGBA32);
                 if (n_staging_buffer == null) {
                     return HostError.CouldNotAllocateStagingBuffer;
                 }
                 break :BUFFER n_staging_buffer;
             } else EBUFFER: {
-                const n_staging_buffer = sdl.SDL_CreateSurfaceFrom(@intCast(info.size.width), @intCast(info.size.height), sdl.SDL_PIXELFORMAT_RGBA32, framebuffer.ptr, @intCast(info.size.width * @sizeOf(u32)));
+                const n_staging_buffer = sdl.SDL_CreateSurfaceFrom(@intCast(info.size.hor), @intCast(info.size.vert), sdl.SDL_PIXELFORMAT_RGBA32, framebuffer.ptr, @intCast(info.size.hor * @sizeOf(u32)));
                 if (n_staging_buffer == null) {
                     return HostError.CouldNotAllocateStagingBuffer;
                 }
@@ -207,9 +256,36 @@ pub fn create_pipeline(comptime pipelineInfo: PipelineInfo) type {
         pub const Surface = struct {
             size: Dim,
             buffer: []FramebufferType,
+            allocator: ?std.mem.Allocator = null,
+
+            pub fn blank(this: This, size: Dim) !@This() {
+                std.debug.assert(size.area() > 0);
+
+                const buffer = try this.allocator.alloc(FramebufferType, size.hor * size.vert);
+                return .{
+                    .allocator = this.allocator,
+                    .size = size,
+                    .buffer = buffer,
+                };
+            }
+
+            pub fn deinit(this: @This()) void {
+                if (this.allocator) |allocator| {
+                    allocator.free(this.buffer);
+                }
+            }
 
             pub inline fn index(this: @This(), x: u32, y: u32) usize {
-                return @as(usize, @intCast(x + y * this.size.width));
+                return @as(usize, @intCast(x + y * this.size.hor));
+            }
+
+            pub fn blit_to(this: @This(), source_region: ?Rect, target: @This(), target_region: ?Rect) !void {
+                const src_rect: Rect = source_region orelse .{ .x = 0, .y = 0, .width = this.size.hor, .height = this.size.vert };
+                const dest_rect: Rect = target_region orelse .{ .x = 0, .y = 0, .width = target.size.hor, .height = target.size.vert };
+                _ = src_rect;
+                _ = dest_rect;
+                //TODO: implement
+                @compileError("Not implemented");
             }
         };
 
